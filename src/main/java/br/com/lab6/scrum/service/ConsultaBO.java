769248512120c2e370834c123e709ba77b8a59bc
Thiago Mendes <machadothiago.tamm@gmail.com>
2015-11-21 15:33:56 -0200

    Criado consultaBO (teste COMMIT do Netbeans)

--- src/main/java/br/com/lab6/scrum/service/ConsultaBO.java
+++ src/main/java/br/com/lab6/scrum/service/ConsultaBO.java
@@ -33,8 +33,8 @@ public class ConsultaBO {
         return conConsultaDAO.list();
     }
     
-    public ConConsulta buscar(){
-        return conConsultaDAO.find(this);
+    public ConConsulta buscar(Object id){
+        return conConsultaDAO.find(id);
     }
     
 }
